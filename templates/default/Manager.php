6beae54a4d5cc10363aa473d7ed3d8a888ecc442
Alvin Woon <alvinwoon@gmail.com>
2006-05-24 19:56:08 +0000

    First phase css implementation

--- templates/default/Manager.php
+++ templates/default/Manager.php
@@ -2,13 +2,32 @@
 <head></head>
 <link rel="stylesheet" type="text/css" media="screen" href="templates/@TEMPLATE@/main.css" />
 <body>
+
+<div id="header"></div><!-- close header -->
+
+<div id="container">
+
+<div id="maintitle">
 <h1>UNL's Event Publishing System</h1>
 <h2>Plan. Publish. Share.</h2>
+</div><!-- maintitle -->
+
+<div id="main_left">
 <div id="navigation">
 <?php echo $this->navigation; ?>
-</div>
+</div><!-- close navigation -->
+
 <div id="maincontentarea">
 <?php UNL_UCBCN::displayRegion($this->output); ?>
-</div>
+</div><!-- close main content area -->
+</div><!-- close main left -->
+
+<div id="right_area">
+dummy
+</div><!-- close right-area -->
+
+
+
+</div><!-- close container -->
 </body>
 </html>
