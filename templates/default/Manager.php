d7171ee0f88dd655f98e4534e99df436c748b1b8
Alvin Woon <alvinwoon@gmail.com>
2006-05-25 14:01:43 +0000

    add heading for each three sections

--- templates/default/Manager.php
+++ templates/default/Manager.php
@@ -7,26 +7,26 @@
 
 <div id="container">
 
-<div id="maintitle">
-<h1>UNL's Event Publishing System</h1>
-<h2>Plan. Publish. Share.</h2>
-</div><!-- maintitle -->
-
-<div id="main_left">
-<div id="navigation">
-<?php echo $this->navigation; ?>
-</div><!-- close navigation -->
-
-<div id="maincontentarea">
-<?php UNL_UCBCN::displayRegion($this->output); ?>
-</div><!-- close main content area -->
-</div><!-- close main left -->
-
-<div id="right_area">
-<?php echo $this->accountright; ?>
-</div><!-- close right-area -->
-
-
+	<div id="maintitle">
+		<h1>UNL's Event Publishing System</h1>
+		<h2>Plan. Publish. Share.</h2>
+	</div><!-- maintitle -->
+	
+	<div id="main_left">
+		<div id="navigation">
+		<h3>Navigation</h3>
+		<?php echo $this->navigation; ?>
+		</div><!-- close navigation -->
+		<div id="maincontentarea">
+		<h3>Main Screen</h3>
+		<?php UNL_UCBCN::displayRegion($this->output); ?>
+		</div><!-- close main content area -->
+	</div><!-- close main left -->
+	
+	<div id="right_area">
+	<h3>Account</h3>
+	<?php echo $this->accountright; ?>
+	</div><!-- close right-area -->
 
 </div><!-- close container -->
 </body>
