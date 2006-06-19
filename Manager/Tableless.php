9904cb39529b3228def803ef4e4f383fb2ac8290
Brett Bieber <brett.bieber@gmail.com>
2006-06-19 15:58:48 +0000

    Tableless form renderer updates.... now uses fieldsets separated by form headers, legend tags wrap form headers and for= and id= are now set.

--- Manager/Tableless.php
+++ Manager/Tableless.php
@@ -27,57 +27,8 @@ require_once 'HTML/QuickForm/Renderer/Default.php';
  * A renderer for HTML_QuickForm that only uses XHTML and CSS but no
  * table tags
  * 
- * You need to specify a stylesheet like the following in your page:
- * <style type="text/css">
- *  form {
- *    margin: 0;
- *    padding: 0;
- *  }
- *  
- *  form br {
- *    display: none;
- *  }
- *  
- *  form label { 
- *    display: block;
- *    float: left;
- *    width: 150px;
- *    padding: 0;
- *    margin: 0 0 5px;
- *    text-align: right;
- *  }
- *  
- *  form input, form textarea {
- *    width: auto;
- *    margin: 0 0 5px 10px;
- *  }
- *  
- *  form span.error {
- *    color: red;
- *    margin: 5px 0 0 10px;
- *  }
- *  
- *  form div.error {
- *    border: 1px solid red;
- *    margin-bottom: 10px;
- *  }
- *  
- *  form div.error br {
- *    display: inline;
- *    clear: left;
- *  }
- *
- *  form div.header {
- *    white-space: nowrap;
- *    background-color: #CCCCCC;
- *    margin-bottom: 10px;
- *    padding: 2px;
- *  }
- *  
- *  textarea {
- *    overflow: auto;
- *  }
- * </style>
+ * You need to specify a stylesheet like the one that you find in
+ * data/stylesheet.css to make this work.
  * 
  * @access public
  */
@@ -89,7 +40,7 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     * @access   private
     */
     var $_headerTemplate = 
-        "\n\t<div class=\"header\"><b>{header}</b></div>";
+        "\n\t\t<legend>{header}</legend>";
 
    /**
     * Element template string
@@ -97,7 +48,7 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     * @access   private
     */
     var $_elementTemplate = 
-        "\n\t<div<!-- BEGIN error --> class=\"error\"<!-- END error -->><!-- BEGIN error -->\n\t\t<label></label><span class=\"error\">{error}</span><br /><!-- END error -->\n\t\t<label><!-- BEGIN required --><span style=\"color: #ff0000\">*</span><!-- END required -->{label}</label>{element}\n\t</div>\n\t<br />";
+        "\n\t\t<label class=\"qflabel\"><!-- BEGIN required --><span style=\"color: #ff0000\">*</span><!-- END required -->{label}</label><div class=\"qfelement<!-- BEGIN error --> error<!-- END error -->\"><!-- BEGIN error --><span class=\"error\">{error}</span><br /><!-- END error -->{element}</div><br />";
 
    /**
     * Form template string
@@ -105,7 +56,21 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     * @access   private
     */
     var $_formTemplate = 
-        "\n<form{attributes}>\n{hidden}\n{content}\n\n</form>";
+        "\n<form{attributes}>\n\t{hidden}\n{content}\n</form>";
+
+   /**
+    * Template used when opening a fieldset
+    * @var      string
+    * @access   private
+    */
+    var $_openFieldsetTemplate = "\n\t<fieldset>";
+
+   /**
+    * Template used when closing a fieldset
+    * @var      string
+    * @access   private
+    */
+    var $_closeFieldsetTemplate = "\n\t</fieldset>";
 
    /**
     * Required Note template string
@@ -113,12 +78,34 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     * @access   private
     */
     var $_requiredNoteTemplate = 
-        "\n{requiredNote}";
+        "\n\t\t{requiredNote}";
 
     function HTML_QuickForm_Renderer_Tableless()
     {
         $this->HTML_QuickForm_Renderer_Default();
     } // end constructor
+
+   /**
+    * Called when visiting a header element
+    *
+    * @param    object     An HTML_QuickForm_header element being visited
+    * @access   public
+    * @return   void
+    */
+    function renderHeader(&$header)
+    {
+        $name = $header->getName();
+        if (!empty($name) && isset($this->_templates[$name])) {
+            $header_html = str_replace('{header}', $header->toHtml(), $this->_templates[$name]);
+        } else {
+            $header_html = str_replace('{header}', $header->toHtml(), $this->_headerTemplate);
+        }
+        if ($this->_fieldsetIsOpen) {
+            $this->_html .= $this->_closeFieldsetTemplate;
+        }
+        $this->_html .= $this->_openFieldsetTemplate . $header_html;
+        $this->_fieldsetIsOpen = true;
+    } // end func renderHeader
 
    /**
     * Helper method for renderElement
@@ -154,7 +141,8 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
             $html = str_replace('<!-- BEGIN error -->', '', $html);
             $html = str_replace('<!-- END error -->', '', $html);
         } else {
-            // the following line contains the only change against default renderer:
+            // the following line contains the only change compared to the default
+            // renderer:
             // we need to match ungreedy (U) here to allow two error blocks
             $html = preg_replace("/([ \t\n\r]*)?<!-- BEGIN error -->(\s|\S)*<!-- END error -->([ \t\n\r]*)?/iU", '', $html);
         }
@@ -171,6 +159,138 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
         }
         return $html;
     } // end func _prepareTemplate
+
+   /**
+    * Renders an element Html
+    * Called when visiting an element
+    *
+    * @param object     An HTML_QuickForm_element object being visited
+    * @param bool       Whether an element is required
+    * @param string     An error message associated with an element
+    * @access public
+    * @return void
+    */
+    function renderElement(&$element, $required, $error)
+    {
+        if (!$this->_inGroup) {
+            $html = $this->_prepareTemplate($element->getName(), $element->getLabel(), $required, $error);
+            // the following lines (until the "elseif") were changed / added
+            // compared to the default renderer
+            $element_html = $element->toHtml();
+            if (!is_null($element->getAttribute('id'))) {
+                $id = $element->getAttribute('id');
+            } else {
+                $id = $element->getName();
+            }
+            $html = str_replace('<label', '<label for="' . $id . '"', $html);
+            $element_html = str_replace('name="' . $id . '"',
+                                        'id="' . $id . '" name="' . $id . '"',
+                                        $element_html);
+            $this->_html .= str_replace('{element}', $element_html, $html);
+
+        } elseif (!empty($this->_groupElementTemplate)) {
+            $html = str_replace('{label}', $element->getLabel(), $this->_groupElementTemplate);
+            if ($required) {
+                $html = str_replace('<!-- BEGIN required -->', '', $html);
+                $html = str_replace('<!-- END required -->', '', $html);
+            } else {
+                $html = preg_replace("/([ \t\n\r]*)?<!-- BEGIN required -->(\s|\S)*<!-- END required -->([ \t\n\r]*)?/i", '', $html);
+            }
+            $this->_groupElements[] = str_replace('{element}', $element->toHtml(), $html);
+
+        } else {
+            $this->_groupElements[] = $element->toHtml();
+        }
+    } // end func renderElement
+
+   /**
+    * Called when visiting a form, before processing any form elements
+    *
+    * @param    object      An HTML_QuickForm object being visited
+    * @access   public
+    * @return   void
+    */
+    function startForm(&$form)
+    {
+        $this->_html = '';
+        $this->_hiddenHtml = '';
+
+        // the following code is new compared to the default renderer
+        $firstVisibleElementIsAHeader = false;
+        $lastElementWasHidden = true;
+        foreach ($form->_elements as $element) {
+            if (is_a($element, 'HTML_QuickForm_Group')) {
+                $this->setGroupTemplate('<div class="qfgroup">{content}</div>', $element->getName());
+            }
+            if ($element->_type != 'hidden' && $lastElementWasHidden == true) {
+                $lastElementWasHidden = false;
+                if ($element->_type == 'header') {
+                    $firstVisibleElementIsAHeader = true;
+                }
+            }
+        }
+        $this->_fieldsetIsOpen = false;
+        if ($firstVisibleElementIsAHeader == false) {
+            $this->_html .= $this->_openFieldsetTemplate;
+            $this->_fieldsetIsOpen = true;
+        }
+    } // end func startForm
+
+   /**
+    * Called when visiting a form, after processing all form elements
+    * Adds required note, form attributes, validation javascript and form content.
+    * 
+    * @param    object      An HTML_QuickForm object being visited
+    * @access   public
+    * @return   void
+    */
+    function finishForm(&$form)
+    {
+        // add a required note, if one is needed
+        if (!empty($form->_required) && !$form->_freezeAll) {
+            $this->_html .= str_replace('{requiredNote}', $form->getRequiredNote(), $this->_requiredNoteTemplate);
+        }
+        if ($this->_fieldsetIsOpen) {
+            $this->_html .= $this->_closeFieldsetTemplate;
+        }
+        // add form attributes and content
+        $html = str_replace('{attributes}', $form->getAttributes(true), $this->_formTemplate);
+        if (strpos($this->_formTemplate, '{hidden}')) {
+            $html = str_replace('{hidden}', $this->_hiddenHtml, $html);
+        } else {
+            $this->_html .= $this->_hiddenHtml;
+        }
+        $this->_hiddenHtml = '';
+        $this->_html = str_replace('{content}', $this->_html, $html);
+        // add a validation script
+        if ('' != ($script = $form->getValidationScript())) {
+            $this->_html = $script . "\n" . $this->_html;
+        }
+    } // end func finishForm
+
+    /**
+     * Sets the template used when opening a fieldset
+     *
+     * @param       string      The HTML used when opening a fieldset
+     * @access      public
+     * @return      void
+     */
+    function setOpenFieldsetTemplate($html)
+    {
+        $this->_openFieldsetTemplate = $html;
+    } // end func setOpenFieldsetTemplate
+
+    /**
+     * Sets the template used when closing a fieldset
+     *
+     * @param       string      The HTML used when closing a fieldset
+     * @access      public
+     * @return      void
+     */
+    function setCloseFieldsetTemplate($html)
+    {
+        $this->_closeFieldsetTemplate = $html;
+    } // end func setCloseFieldsetTemplate
 
 } // end class HTML_QuickForm_Renderer_Default
 ?>
