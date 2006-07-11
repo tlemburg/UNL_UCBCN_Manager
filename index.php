3b1d17ba4be1f1915e464865d199177718750fa7
Brett Bieber <brett.bieber@gmail.com>
2006-07-11 15:12:32 +0000

    Default template is now a php unl template.

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
  */
 require_once 'UNL/UCBCN/Manager.php';
 require_once 'Auth.php';
+
+$GLOBALS['unl_template_dependents'] = $_SERVER['DOCUMENT_ROOT'];
+
 $a = new Auth('DB',array('dsn'=>'mysql://eventcal:eventcal@localhost/eventcal'),NULL,false);
 
 $frontend = new UNL_UCBCN_Manager(array(	'template'=>'default',
