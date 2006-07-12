44ddcf74655c17f39dfddfbaf4a13165fe9277a4
Brett Bieber <brett.bieber@gmail.com>
2006-07-12 18:08:41 +0000

    Update tableless renderer to latest.

--- Manager/Tableless.php
+++ Manager/Tableless.php
@@ -1,25 +1,27 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP version 4.0                                                      |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Alexey Borzov <borz_off@cs.msu.su>                          |
-// |          Adam Daniel <adaniel1@eesus.jnj.com>                        |
-// |          Bertrand Mansion <bmansion@mamasam.com>                     |
-// |          Mark Wiesemann <wiesemann@php.net>                          |
-// +----------------------------------------------------------------------+
-//
-// $Id $
+/**
+ * A renderer for HTML_QuickForm that only uses XHTML and CSS but no
+ * table tags
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.01 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   HTML
+ * @package    HTML_QuickForm_Renderer_Tableless
+ * @author     Alexey Borzov <borz_off@cs.msu.su>
+ * @author     Adam Daniel <adaniel1@eesus.jnj.com>
+ * @author     Bertrand Mansion <bmansion@mamasam.com>
+ * @author     Mark Wiesemann <wiesemann@php.net>
+ * @copyright  2005-2006 The PHP Group
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    CVS: $Id $
+ * @link       http://pear.php.net/package/[...]
+ */
 
 require_once 'HTML/QuickForm/Renderer/Default.php';
 
@@ -29,8 +31,16 @@ require_once 'HTML/QuickForm/Renderer/Default.php';
  * 
  * You need to specify a stylesheet like the one that you find in
  * data/stylesheet.css to make this work.
- * 
- * @access public
+ *
+ * @category   HTML
+ * @package    HTML_QuickForm_Renderer_Tableless
+ * @author     Alexey Borzov <borz_off@cs.msu.su>
+ * @author     Adam Daniel <adaniel1@eesus.jnj.com>
+ * @author     Bertrand Mansion <bmansion@mamasam.com>
+ * @author     Mark Wiesemann <wiesemann@php.net>
+ * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
+ * @version    Release: 0.3.0
+ * @link       http://pear.php.net/package/[...]
  */
 class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
 {
@@ -48,7 +58,7 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     * @access   private
     */
     var $_elementTemplate = 
-        "\n\t\t<label class=\"qflabel\"><!-- BEGIN required --><span style=\"color: #ff0000\">*</span><!-- END required -->{label}</label><div class=\"qfelement<!-- BEGIN error --> error<!-- END error -->\"><!-- BEGIN error --><span class=\"error\">{error}</span><br /><!-- END error -->{element}</div><br />";
+        "\n\t\t<div class=\"qfrow\"><label class=\"qflabel\"><!-- BEGIN required --><span style=\"color: #ff0000\">*</span><!-- END required -->{label}</label><div class=\"qfelement<!-- BEGIN error --> error<!-- END error -->\"><!-- BEGIN error --><span class=\"error\">{error}</span><br /><!-- END error -->{element}</div></div><br />";
 
    /**
     * Form template string
@@ -80,6 +90,26 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     var $_requiredNoteTemplate = 
         "\n\t\t{requiredNote}";
 
+   /**
+    * Whether a fieldset tag is open or not
+    * @var      bool
+    * @access   private
+    */
+   var $_fieldsetIsOpen = false;
+
+   /**
+    * Array of element names that indicate the end of a fieldset
+    * (a new one will be opened when a the next header element occurs)
+    * @var      array
+    * @access   private
+    */
+    var $_stopFieldsetElements = array();
+
+   /**
+    * Constructor
+    *
+    * @access public
+    */
     function HTML_QuickForm_Renderer_Tableless()
     {
         $this->HTML_QuickForm_Renderer_Default();
@@ -95,7 +125,10 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     function renderHeader(&$header)
     {
         $name = $header->getName();
-        if (!empty($name) && isset($this->_templates[$name])) {
+        if (is_null($header->_text)) {
+            $header_html = '';
+        }
+        elseif (!empty($name) && isset($this->_templates[$name])) {
             $header_html = str_replace('{header}', $header->toHtml(), $this->_templates[$name]);
         } else {
             $header_html = str_replace('{header}', $header->toHtml(), $this->_headerTemplate);
@@ -106,59 +139,6 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
         $this->_html .= $this->_openFieldsetTemplate . $header_html;
         $this->_fieldsetIsOpen = true;
     } // end func renderHeader
-
-   /**
-    * Helper method for renderElement
-    *
-    * @param    string      Element name
-    * @param    mixed       Element label (if using an array of labels, you should set the appropriate template)
-    * @param    bool        Whether an element is required
-    * @param    string      Error message associated with the element
-    * @access   private
-    * @see      renderElement()
-    * @return   string      Html for element
-    */
-    function _prepareTemplate($name, $label, $required, $error)
-    {
-        if (is_array($label)) {
-            $nameLabel = array_shift($label);
-        } else {
-            $nameLabel = $label;
-        }
-        if (isset($this->_templates[$name])) {
-            $html = str_replace('{label}', $nameLabel, $this->_templates[$name]);
-        } else {
-            $html = str_replace('{label}', $nameLabel, $this->_elementTemplate);
-        }
-        if ($required) {
-            $html = str_replace('<!-- BEGIN required -->', '', $html);
-            $html = str_replace('<!-- END required -->', '', $html);
-        } else {
-            $html = preg_replace("/([ \t\n\r]*)?<!-- BEGIN required -->(\s|\S)*<!-- END required -->([ \t\n\r]*)?/i", '', $html);
-        }
-        if (isset($error)) {
-            $html = str_replace('{error}', $error, $html);
-            $html = str_replace('<!-- BEGIN error -->', '', $html);
-            $html = str_replace('<!-- END error -->', '', $html);
-        } else {
-            // the following line contains the only change compared to the default
-            // renderer:
-            // we need to match ungreedy (U) here to allow two error blocks
-            $html = preg_replace("/([ \t\n\r]*)?<!-- BEGIN error -->(\s|\S)*<!-- END error -->([ \t\n\r]*)?/iU", '', $html);
-        }
-        if (is_array($label)) {
-            foreach($label as $key => $text) {
-                $key  = is_int($key)? $key + 2: $key;
-                $html = str_replace("{label_{$key}}", $text, $html);
-                $html = str_replace("<!-- BEGIN label_{$key} -->", '', $html);
-                $html = str_replace("<!-- END label_{$key} -->", '', $html);
-            }
-        }
-        if (strpos($html, '{label_')) {
-            $html = preg_replace('/\s*<!-- BEGIN label_(\S+) -->.*<!-- END label_\1 -->\s*/i', '', $html);
-        }
-        return $html;
-    } // end func _prepareTemplate
 
    /**
     * Renders an element Html
@@ -172,6 +152,11 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     */
     function renderElement(&$element, $required, $error)
     {
+        // if the element names indicates the end of a fieldset, close the fieldset
+        if (in_array($element->getName(), $this->_stopFieldsetElements)) {
+            $this->_html .= $this->_closeFieldsetTemplate;
+            $this->_fieldsetIsOpen = false;
+        }
         if (!$this->_inGroup) {
             $html = $this->_prepareTemplate($element->getName(), $element->getLabel(), $required, $error);
             // the following lines (until the "elseif") were changed / added
@@ -204,39 +189,6 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     } // end func renderElement
 
    /**
-    * Called when visiting a form, before processing any form elements
-    *
-    * @param    object      An HTML_QuickForm object being visited
-    * @access   public
-    * @return   void
-    */
-    function startForm(&$form)
-    {
-        $this->_html = '';
-        $this->_hiddenHtml = '';
-
-        // the following code is new compared to the default renderer
-        $firstVisibleElementIsAHeader = false;
-        $lastElementWasHidden = true;
-        foreach ($form->_elements as $element) {
-            if (is_a($element, 'HTML_QuickForm_Group')) {
-                $this->setGroupTemplate('<div class="qfgroup">{content}</div>', $element->getName());
-            }
-            if ($element->_type != 'hidden' && $lastElementWasHidden == true) {
-                $lastElementWasHidden = false;
-                if ($element->_type == 'header') {
-                    $firstVisibleElementIsAHeader = true;
-                }
-            }
-        }
-        $this->_fieldsetIsOpen = false;
-        if ($firstVisibleElementIsAHeader == false) {
-            $this->_html .= $this->_openFieldsetTemplate;
-            $this->_fieldsetIsOpen = true;
-        }
-    } // end func startForm
-
-   /**
     * Called when visiting a form, after processing all form elements
     * Adds required note, form attributes, validation javascript and form content.
     * 
@@ -246,26 +198,10 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     */
     function finishForm(&$form)
     {
-        // add a required note, if one is needed
-        if (!empty($form->_required) && !$form->_freezeAll) {
-            $this->_html .= str_replace('{requiredNote}', $form->getRequiredNote(), $this->_requiredNoteTemplate);
-        }
         if ($this->_fieldsetIsOpen) {
             $this->_html .= $this->_closeFieldsetTemplate;
         }
-        // add form attributes and content
-        $html = str_replace('{attributes}', $form->getAttributes(true), $this->_formTemplate);
-        if (strpos($this->_formTemplate, '{hidden}')) {
-            $html = str_replace('{hidden}', $this->_hiddenHtml, $html);
-        } else {
-            $this->_html .= $this->_hiddenHtml;
-        }
-        $this->_hiddenHtml = '';
-        $this->_html = str_replace('{content}', $this->_html, $html);
-        // add a validation script
-        if ('' != ($script = $form->getValidationScript())) {
-            $this->_html = $script . "\n" . $this->_html;
-        }
+        parent::finishForm($form);
     } // end func finishForm
 
     /**
@@ -291,6 +227,24 @@ class HTML_QuickForm_Renderer_Tableless extends HTML_QuickForm_Renderer_Default
     {
         $this->_closeFieldsetTemplate = $html;
     } // end func setCloseFieldsetTemplate
+
+    /**
+     * Adds one or more element names that indicate the end of a fieldset
+     * (a new one will be opened when a the next header element occurs)
+     *
+     * @param       mixed      Element name(s) (as array or string)
+     * @access      public
+     * @return      void
+     */
+    function addStopFieldsetElements($element)
+    {
+        if (is_array($element)) {
+            $this->_stopFieldsetElements = array_merge($this->_stopFieldsetElements,
+                                                       $element);
+        } else {
+            $this->_stopFieldsetElements[] = $element;
+        }
+    } // end func addStopFieldsetElements
 
 } // end class HTML_QuickForm_Renderer_Default
 ?>
