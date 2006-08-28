65344d2f03c17c07c6e2d6816fdf0de6378dbc01
Alvin Woon <alvinwoon@gmail.com>
2006-08-28 21:09:49 +0000

    adjust positioning for 'forget your password'.

--- templates/default/Manager_Login.tpl.php
+++ templates/default/Manager_Login.tpl.php
@@ -4,8 +4,8 @@
 	$form->addElement('text',$this->user_field,'User');
 	$form->addElement('password',$this->password_field,'Password');
 	$form->addElement('xbutton','submit','Submit','type="submit"');
+	$form->addElement('static','','','<a href="http://my.unl.edu/webapps/blackboard/password" title="" id="forgot">(Forgot your password?)</a>');
 	$renderer =& new HTML_QuickForm_Renderer_Tableless();
 	$form->accept($renderer);
 	echo $renderer->toHtml();
 ?>
-<p id="lost"><a href="http://my.unl.edu/webapps/blackboard/password" title="" id="forgot">Forgot your password?</a></p>
