2fc501472c29bcc4633a7c897f4acea959d5657f
Brett Bieber <brett.bieber@gmail.com>
2006-12-11 16:53:31 +0000

    fix grammatical error on the login.. remove comma, add period.

--- templates/default/Manager_Login.tpl.php
+++ templates/default/Manager_Login.tpl.php
@@ -1,4 +1,5 @@
-<p>Welcome to the University Event Publishing System, please log in using your My.UNL (Blackboard/LDAP) Username and Password.</p>
+<p>Welcome to the University Event Publishing System.<br />Please log in using your My.UNL (Blackboard/LDAP) Username and Password.</p>
+
 <?php
 	$form = new HTML_QuickForm('login');
 	$form->addElement('text',$this->user_field,'User');
